7e2ab904f73800cfa3efb7f8b1c5663075f14ca4
Mongus <mongus.c@gmail.com>
2021-12-08 11:49:00 +0900

    add GET /:somthing

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ app.get('/', (req, res) => {
     res.send('hello world!')
 })
 
+app.get('/:something', (req, res) => {
+    const { something } = req.params
+
+    res.send(`your message : ${something}`)
+})
+
 server.listen(port, function () {
     console.log('[system] Open | Port : ' + port)
 })
